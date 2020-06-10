83d125d882ad1647cdc05d8ac0f14ddd45a9b447
IvanS <sobolev-ivan901@yandex.ru>
2020-06-10 19:34:10 +0300

    add params to post

--- src/test/java/dins/com/post/Tests_POST.java
+++ src/test/java/dins/com/post/Tests_POST.java
@@ -3,8 +3,15 @@ package dins.com.post;
 import com.google.gson.Gson;
 import dins.com.endpoints.Endpoint;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
@@ -12,9 +19,12 @@ import static org.hamcrest.Matchers.*;
 
 public class Tests_POST {
 
+    private int id;
+
     @Test
-    public void test_POST() {
-        String bodyOfRequest = getJsonString("Arkady");
+    @Parameters({"field", "valueField"})
+    public void test_POST_createUser_withRequiredField_positive(String field, String valueField) {
+        String bodyOfRequest = getJsonString(field ,valueField);
         given().contentType(ContentType.JSON).accept(ContentType.JSON).
                 body(bodyOfRequest).
                 when().
@@ -22,13 +32,22 @@ public class Tests_POST {
                 then().
                 assertThat().
                 statusCode(201).and().
-                body("id", equalTo(10));
+                body(field, equalTo(valueField));
     }
 
-    private String getJsonString(String firstNameValue){
+    @AfterMethod
+    public void delete_testing_items(){
+        Response e = given().get(Endpoint.users);
+        List<Integer> a = e.jsonPath().getList("id");
+        when().
+            delete(Endpoint.users + "/" + a.get(a.size()-1));
+        System.out.println("Was deleted");
+    }
+
+    private String getJsonString(String field, Object valueField){
         Gson gson = new Gson();
         Map<String, Object> mapa = new HashMap<>();
-        mapa.put("firstName", firstNameValue);
+        mapa.put(field, valueField);
         return gson.toJson(mapa);
     }
 }
---
+++ src/test/java/dins/com/put/Tests_PUT.java
@@ -0,0 +1,9 @@
+package dins.com.put;
+
+import org.testng.annotations.Test;
+
+public class Tests_PUT {
+
+    @Test
+    public void test_PUT (){}
+}
