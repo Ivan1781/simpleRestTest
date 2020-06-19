09fff97e445ecd5dea7ff3d7be5b281267a70c22
IvanS <sobolev-ivan901@yandex.ru>
2020-06-19 13:31:46 +0300

    delete

--- src/test/java/dins/com/endpoints/Endpoint.java
+++
@@ -1,7 +0,0 @@
-package dins.com.endpoints;
-
-public final class Endpoint {
-    public static final String host = "http://localhost";
-    public static final Integer port = 8080;
-    public static final String users = "/users";
-}
